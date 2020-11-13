167cfaff59b0ad80e81c306e8db263c5854fcec0
francesco <f.iacono89@gmail.com>
2020-11-13 17:57:06 +0100

    js

--- js/script.js
+++ js/script.js
@@ -12,23 +12,17 @@ bottoneGenera.addEventListener( 'click',
     function() {
       // nome del passeggero
       var nomeDelPasseggero = document.getElementById('nome_cognome').value;
-      console.log(nomeDelPasseggero);
       // km da percorrere
       var numeroKm = document.getElementById('km').value;
-      console.log(numeroKm);
       // eta del passeggero
       var eta = document.getElementById('fascia_eta').value;
-      console.log(eta);
       // prezzo table_biglietto
       var prezzoPerKm = 0.21;
       var prezzoStandard = numeroKm * prezzoPerKm;
-      console.log(prezzoStandard);
       // sconto del 20% per i minorenni
       var scontoUnder = (prezzoStandard * 20 / 100);
-      console.log(scontoUnder);
       // sconto del 40% over 65
       var scontoOver = (prezzoStandard * 40 / 100);
-      console.log(scontoOver);
       // messaggio sconto
       var messaggio = "Biglietto Standard";
       // applicare lo sconto
@@ -37,20 +31,16 @@ bottoneGenera.addEventListener( 'click',
           document.getElementById('offerta_passeggero').innerHTML = messaggio;
           var prezzoFinale = prezzoStandard - scontoUnder;
           prezzoFinale = prezzoFinale.toFixed(2);
-          console.log(prezzoFinale);
           document.getElementById('costo_biglietto').innerHTML = prezzoFinale + " €";
       } else if (eta == "over 65") {
           messaggio = "Offerta Over 65";
           document.getElementById('offerta_passeggero').innerHTML = messaggio;
           var prezzoFinale = prezzoStandard - scontoOver;
           prezzoFinale = prezzoFinale.toFixed(2);
-          console.log(prezzoFinale);
           document.getElementById('costo_biglietto').innerHTML =  prezzoFinale + " €";
-
       } else {
           document.getElementById('offerta_passeggero').innerHTML = messaggio;
           document.getElementById('costo_biglietto').innerHTML =  prezzoStandard + " €";
-          console.log(prezzoStandard);
       }
 
       // dati del biglietto
@@ -59,5 +49,17 @@ bottoneGenera.addEventListener( 'click',
       document.getElementById('carrozza_passeggero').innerHTML = carrozzaPassegero;
       var codiceCP = Math.floor( Math.random() * 10000) + 90000;
       document.getElementById('codice_passeggero').innerHTML = codiceCP;
+
+      // biglietto show
+      var bigliettoTreno = document.getElementById('biglietto');
+      bigliettoTreno.className = 'show'
     }
+  );
+
+  var bottoneAnnulla = document.getElementById('annulla');
+  bottoneAnnulla.addEventListener( 'click',
+      function() {
+        var bigliettoTreno = document.getElementById('biglietto');
+        bigliettoTreno.className = 'hidden';
+      }
   );
